e1ee83cd026213fd2715674b9140cdf6d7d48358
Zoe Amorime <zoealcantara03@outlook.com>
2025-02-09 16:53:29 -0300

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,16 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./app/**/*.tsx'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        brandColor: '#EB356E',
+        brandColorShadow: '#F45B87',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
